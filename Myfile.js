ed4be85052f960070432e3c50f92f873e6274a5d
Naveen001 <111596908+NaveenkumarHulimani@users.noreply.github.com>
2024-08-08 11:37:32 +0530

    Update Myfile.js

--- Myfile.js
+++ Myfile.js
@@ -1,1 +1,27 @@
-console.log("Hello word");
+// Myfile.js
+
+console.log("Hello world");
+
+// Simple test function
+function test() {
+  if (console.log.calledWith("Hello world")) {
+    console.log("Test Passed");
+  } else {
+    console.log("Test Failed");
+  }
+}
+
+// Mock console.log to capture outputs for testing
+console.log = (function() {
+  const original = console.log;
+  const logs = [];
+  function mock() {
+    logs.push(arguments);
+    original.apply(console, arguments);
+  }
+  mock.calledWith = (msg) => logs.some(log => log.includes(msg));
+  mock.restore = () => console.log = original;
+  return mock;
+})();
+
+test();
